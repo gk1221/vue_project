c189b61afba2e8585d352147088ddf57f563967c
myself <good125628@gmail.com>
2022-05-26 01:46:02 +0800

    修正路徑

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/gk1221 / vue_project/dist'
+  publicPath: '/gk1221/vue_project/dist'
 })
